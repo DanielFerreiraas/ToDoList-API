2cc8779cc11110b2863806616dc34fb0ebbe28e1
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-12-14 23:04:03 -0300

    CRUD ToDoList
    Bônus de documentação de rotas com swagger

---
+++ src/test/java/br/com/danielferreira/todolist/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielferreira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
